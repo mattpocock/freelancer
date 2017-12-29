b0f2f3cc5488cead8089fe58b7883a099c49c838
mattpocock <mattpocockvoice@gmail.com>
2017-12-29 16:35:41 +0000

    Initial Setup

---
+++ wp-content/themes/burfitt/index.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="col-8 align-center puzzle-area__container">
+        <h1>What are you looking for?</h1>
+
+        <div class="col-12 puzzle-area">
+            <div class="col-6 col-xs-12 puzzle-area__piece">
+                <h2>Translation</h2>
+                <p>Lorem Impsum epi jhasd hb asdbh  h hbasdb hasd b asd jhasd basjd jhas dbasdb ha sbhsadhba sjha basdh jhas basdjhbasd</p>
+            </div>
+            <div class="col-6 col-xs-12 puzzle-area__piece">
+                <h2>Editing</h2>
+                <p>Lorem Impsum epi jhasd hb asdbh  h hbasdb hasd b asd jhasd basjd jhas dbasdb ha sbhsadhba sjha basdh jhas basdjhbasd</p>
+            </div>
+            <div class="col-6 col-xs-12 puzzle-area__piece">
+                <h2>Photography</h2>
+                <p>Lorem Impsum epi jhasd hb asdbh  h hbasdb hasd b asd jhasd basjd jhas dbasdb ha sbhsadhba sjha basdh jhas basdjhbasd</p>
+            </div>
+            <div class="col-6 col-xs-12 puzzle-area__piece">
+                <h2>Writing</h2>
+                <p>Lorem Impsum epi jhasd hb asdbh  h hbasdb hasd b asd jhasd basjd jhas dbasdb ha sbhsadhba sjha basdh jhas basdjhbasd</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
