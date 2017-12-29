754171c0891417d4742dc32eebe8359d87c96772
mattpocock <mattpocockvoice@gmail.com>
2017-12-29 16:35:41 +0000

    Initial Setup

---
+++ wp-content/themes/burfitt/header.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Louise Burfitt</title>
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700|Hind:300" rel="stylesheet">
+    <?php wp_head();?>
+</head>
+
+<body>
+    
+<div class="container">
+
+<div class="col-5 col-xs-12 title-area">
+    <h1>Louise Burfitt</h1>
+    <h2>Translator, Editor, Writer, Photographer</h2>
+</div>
+
+<div class="col-7 nav-bar hidden-xs">
+    <nav class="blog-nav">
+        <a class="blog-nav-item active" href="#">Home</a>
+        <a class="blog-nav-item" href="#">New features</a>
+        <a class="blog-nav-item" href="#">Press</a>
+        <a class="blog-nav-item" href="#">New hires</a>
+        <a class="blog-nav-item" href="#">About</a>
+    </nav>
+</div>
+
+</div>
