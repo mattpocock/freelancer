0e8220d16eadeab61af03e5b917b2c782afd8bfd
mattpocock <mattpocockvoice@gmail.com>
2018-01-15 12:19:17 +0000

    Custom Shortcodes
    Customisability: Each page can have a customised heading.

---
+++ wp-content/plugins/freelancer-functions/freelancer-functions.php
@@ -0,0 +1,24 @@
+<?php
+/*
+* Plugin Name: Freelancer Functions
+* Description: Shortcodes for Freelancer
+* Author: Matt Pocock
+* Author URI: https://mattpocock.github.io
+*/
+
+// Example 1 : WP Shortcode to display form on any page or post.
+function page_top_image( $atts ) {
+
+    $a = shortcode_atts( array(
+        'src' => '',
+        'title' => ''
+    ), $atts);
+
+    return '<h2 class="page-heading">'.$a['title'].'</h2>'
+            .'<img class="page-heading__image" src="'.$a["src"].'"/>';
+
+}
+
+add_shortcode('featured-image', 'page_top_image');
+
+?>
